6138f9bb10d86cd00238be3d118dc9215ebdd468
unknown <gabebarraza33@gmail.com>
2023-11-15 18:33:56 -0600

    Adding var arrays to the script.js file

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var characterLength = 10;
+var choiceArr = []
+
+var specialCharArr = ["!", "@", "#", "$", "%", "^", "&", "*", "*", "(", ")", "_", "-", "+", "="]
+var lowercaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", " ", " ", " ", " ", " ", " ", " ", " ", " ", ]
+var uppercaseArr = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", ]
+var numberArr = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", ]
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
